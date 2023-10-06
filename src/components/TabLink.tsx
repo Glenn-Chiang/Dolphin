3a54588c5f0785bc8979526b7e80a75bc96588b9
Glenn Chiang <glennchiang24@gmail.com>
2023-10-06 20:06:06 +0800

    Reverted Tablink to determine active route using equality operator to avoid conflicting different route levels

--- src/components/TabLink.tsx
+++ src/components/TabLink.tsx
@@ -8,7 +8,7 @@ type TabLinkProps = {
 };
 
 export default function TabLink({ activePath, href, children }: TabLinkProps) {
-  const isActive = activePath.startsWith(href)
+  const isActive = activePath === href
   return (
     <Link
       href={href}
