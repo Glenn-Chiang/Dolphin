9a09de357bcf73bfbfb3ae41e720ca778c76c995
Glenn Chiang <glennchiang24@gmail.com>
2023-10-07 12:24:55 +0800

    Disabled spellcheck on input elements globally

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" spellCheck='false'>
       <body>
         <Navbar />
         <div className="mt-20 mb-10 px-4">{children}</div>
