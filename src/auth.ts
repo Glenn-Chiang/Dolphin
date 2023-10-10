57ca14b54f68e206c87a4bd2511003dc9db2fce3
Glenn Chiang <glennchiang24@gmail.com>
2023-10-10 22:53:40 +0800

    Implemented getCurrentUser method for retrieving current userId on server side

--- src/auth.ts
+++ src/auth.ts
@@ -1,25 +1,24 @@
 import { useSession } from "next-auth/react";
 import { getServerSession } from "next-auth/next";
+import { authOptions } from "./app/api/auth/[...nextauth]/route";
 
 // Get current user in client components
 const useCurrentUser = () => {
   const session = useSession();
   const user = session.data?.user;
-  
-  if (!user) return null
-
-  const userId:number = (user as any).id
+  if (!user) return null;
+  const userId: number = (user as any).id; // ewww
   // console.log("UserId:", userId);
   return userId;
 };
 
 // Get current user in server components / server actions
 const getCurrentUser = async () => {
-  // const session = await getServerSession()
-  // const userId = session?.user
-  // console.log("UserId:", userId)
-  // return userId
-  return 1
-}
+  const session = await getServerSession(authOptions);
+  const user = session?.user;
+  if (!user) return null;
+  const userId = (user as any).id; // ewww
+  return userId;
+};
 
-export {useCurrentUser, getCurrentUser };
+export { useCurrentUser, getCurrentUser };
