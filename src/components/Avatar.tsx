be1bb533e6d8d3cd8b619a938be65d1655449229
Glenn Chiang <glennchiang24@gmail.com>
2023-10-12 12:11:08 +0800

    Replaced profile icons with avatars

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -4,13 +4,17 @@ import { User } from "@prisma/client";
 import Image from "next/image";
 
 export default function Avatar({ user }: { user: User | null }) {
-  return (
-    <div>
-      {user && user.avatarSource ? (
-        <Image src={user.avatarSource} alt="" width={40} height={40}/>
-      ) : (
-        <FontAwesomeIcon icon={faUserCircle} />
-      )}  
-    </div>
-  );
+  if (user && user.avatarSource) {
+    return (
+      <Image
+        src={user.avatarSource}
+        alt=""
+        width={40}
+        height={40}
+        className="rounded-full"
+      />
+    );
+  } else {
+    return <FontAwesomeIcon icon={faUserCircle} />;
+  }
 }
