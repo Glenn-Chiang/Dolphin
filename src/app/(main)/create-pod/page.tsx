dfd28c36cd828afa98bdecf7a0861dd96edafa35
Glenn Chiang <glennchiang24@gmail.com>
2023-10-13 18:12:23 +0800

    Added client-side form validation for createPod

--- src/app/(main)/create-pod/page.tsx
+++ src/app/(main)/create-pod/page.tsx
@@ -1,27 +1,60 @@
+"use client";
+
 import { SubmitButton } from "@/components/buttons";
 import { createPod } from "@/actions/pods";
+import { SubmitHandler, useForm } from "react-hook-form";
+import FormError from "@/components/FormError";
+
+type FormValues = {
+  name: string;
+  about: string;
+};
 
 export default function CreatePod() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>();
+
+  const onSubmit: SubmitHandler<FormValues> = (formValues) => {};
+
   return (
     <main className="bg-white p-4 rounded-xl shadow">
       <h1>Create a Pod!</h1>
-      <form action={createPod} className="py-4 flex flex-col gap-4">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        action={createPod}
+        className="py-4 flex flex-col gap-4"
+      >
         <div className="flex flex-col gap-2">
           <label htmlFor="name">Name</label>
           <input
             id="name"
-            name="name"
-            className="shadow bg-slate-50 rounded-md p-2"
+            {...register("name", {
+              required: "Pod name cannot be empty",
+              maxLength: {
+                value: 25,
+                message: "Pod name cannot be longer than 25 characters",
+              },
+            })}
           />
         </div>
+        {errors.name && <FormError>{errors.name.message}</FormError>}
+
         <div className="flex flex-col gap-2">
           <label htmlFor="about">About</label>
           <input
             id="about"
-            name="about"
-            className="shadow bg-slate-50 rounded-md p-2"
+            {...register("about", {
+              maxLength: {
+                value: 500,
+                message: "About cannot be longer than 500 characters",
+              },
+            })}
           />
         </div>
+        {errors.about && <FormError>{errors.about.message}</FormError>}
         <div className="flex gap-2">
           <SubmitButton>Create</SubmitButton>
         </div>
