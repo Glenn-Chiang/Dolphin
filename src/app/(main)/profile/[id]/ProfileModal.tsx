fb6c685b31be3020fb7fee489311e41aa1401394
Glenn Chiang <glennchiang24@gmail.com>
2023-10-13 12:32:23 +0800

    Styled ProfileModal

--- src/app/(main)/profile/[id]/ProfileModal.tsx
+++ src/app/(main)/profile/[id]/ProfileModal.tsx
@@ -1,11 +1,18 @@
 "use client";
 
-import { SubmitHandler, useForm } from "react-hook-form";
-import Modal from "@/components/Modal";
-import { SubmitButton } from "@/components/buttons";
 import { updateProfile } from "@/actions/users";
 import FormError from "@/components/FormError";
+import Modal from "@/components/Modal";
+import { SubmitButton } from "@/components/buttons";
+import {
+  faImage,
+  faInfoCircle,
+  faUser,
+  faUserEdit,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { User } from "@prisma/client";
+import { SubmitHandler, useForm } from "react-hook-form";
 
 type ProfileModalProps = {
   close: () => void;
@@ -33,40 +40,50 @@ export default function ProfileModal({ close, user }: ProfileModalProps) {
 
   return (
     <Modal close={close}>
-      <h1>Edit profile</h1>
+      <h1 className="flex gap-2 items-center">
+        <FontAwesomeIcon icon={faUserEdit} />
+        Edit profile
+      </h1>
       <form
         onSubmit={handleSubmit(onSubmit)}
         className="py-4 flex flex-col gap-4"
       >
-        <div className="flex flex-col gap-2">
-          <label htmlFor="name">Name</label>
-          <input
-            id="name"
-            defaultValue={user.name}
-            {...register("name", {
-              required: "Your name can't be empty",
-              maxLength: {
-                value: 25,
-                message: "Your name can't be longer than 25 characters",
-              },
-            })}
-          />
-          {errors.name && <FormError>{errors.name.message}</FormError>}
-          <label htmlFor="about">About</label>
-          <textarea
-            {...register("about", {
-              maxLength: {
-                value: 500,
-                message: "Your About can't be longer than 500 characters",
-              },
-            })}
-            defaultValue={user.about}
-            id="about"
-            className="p-2 shadow bg-slate-100"
-          />
-        </div>
+        <label htmlFor="name" className="flex gap-2 items-center">
+          <FontAwesomeIcon icon={faUser} />
+          Name
+        </label>
+        <input
+          id="name"
+          defaultValue={user.name}
+          {...register("name", {
+            required: "Your name can't be empty",
+            maxLength: {
+              value: 25,
+              message: "Your name can't be longer than 25 characters",
+            },
+          })}
+        />
+        {errors.name && <FormError>{errors.name.message}</FormError>}
+        <label htmlFor="about" className="flex gap-2 items-center">
+          <FontAwesomeIcon icon={faInfoCircle} />
+          About
+        </label>
+        <textarea
+          {...register("about", {
+            maxLength: {
+              value: 500,
+              message: "Your About can't be longer than 500 characters",
+            },
+          })}
+          defaultValue={user.about}
+          id="about"
+          className="p-2 shadow bg-slate-100"
+        />
         {errors.about && <FormError>{errors.about.message}</FormError>}
-        <label htmlFor="avatar">Avatar image link</label>
+        <label className="flex gap-2 items-center" htmlFor="avatar">
+          <FontAwesomeIcon icon={faImage} />
+          Avatar image link
+        </label>
         <input
           id="avatar"
           {...register("avatarSrc")}
