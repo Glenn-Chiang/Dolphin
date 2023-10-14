964f9a287c497527de6e6e11521115728eb1c188
Glenn Chiang <glennchiang24@gmail.com>
2023-10-14 11:57:18 +0800

    Added custom pages to middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,2 +1,8 @@
-export {default} from 'next-auth/middleware'
-export const config = {matcher: ['/']}
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+  pages: {
+    signIn: "/login",
+    signOut: "/logout",
+  },
+});
