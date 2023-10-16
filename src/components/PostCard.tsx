af3bea2ca756e3c2a350fd2283b05de3834f7947
Glenn Chiang <glennchiang24@gmail.com>
2023-10-16 21:43:02 +0800

    Adjusted image sizing

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -54,31 +54,37 @@ export default function PostCard({ post }: { post: PostDetail }) {
   return (
     <article className="bg-white p-4 pb-2 rounded-md relative shadow hover:shadow-lg transition">
       <Link href={`/post/${post.id}`}>
-        <Link
-          href={`/pods/${post.podId}`}
-          className="text-sky-500 font-medium -m-2 p-2 flex gap-2 items-center w-max rounded-xl hover:bg-sky-200"
-        >
-          <PodIcon src={post.pod.iconSource} />
-          {post.pod.name}
-        </Link>
-        <h2 className="py-4">{post.title}</h2>
         <div className="flex gap-4 text-slate-500 items-center ">
           <Link
-            href={`/profile/${post.authorId}`}
-            className="flex gap-2 items-center hover:text-sky-500"
+            href={`/pods/${post.podId}`}
+            className="text-sky-500 font-medium -m-2 p-2 flex gap-2 items-center w-max rounded-xl hover:bg-sky-200"
           >
-            <Avatar user={post.author} />
-            {post.author ? post.author.name : "[deleted]"}
+            <PodIcon src={post.pod.iconSource} />
+            {post.pod.name}
           </Link>
-          <span>{DateTime.fromJSDate(post.createdAt).toRelative() }</span>
+          <span>{DateTime.fromJSDate(post.createdAt).toRelative()}</span>
         </div>
+        <h2 className="py-4">{post.title}</h2>
+        <Link
+          href={`/profile/${post.authorId}`}
+          className="flex gap-2 items-center hover:text-sky-500"
+        >
+          <Avatar user={post.author} />
+          {post.author ? post.author.name : "[deleted]"}
+        </Link>
         <div className="py-2 whitespace-pre-wrap">{post.content}</div>
-        
-        {post.imageUrl &&
-        <div className="flex justify-center rounded-md p-2">
-          <Image src={post.imageUrl} alt="" width={400} height={400} className="rounded-2xl w-full h-auto"/>
-        </div>
-        }
+
+        {post.imageUrl && (
+          <div className="rounded-md p-2">
+            <Image
+              src={post.imageUrl}
+              alt=""
+              width={400}
+              height={400}
+              className="rounded-2xl w-full object-contain max-h-[50vh]"
+            />
+          </div>
+        )}
 
         <div className="flex gap-4 py-2">
           <LikeButton liked={liked} likes={likes} onClick={handleLikeClick} />
