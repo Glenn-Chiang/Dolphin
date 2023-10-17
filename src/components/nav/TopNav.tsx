1e7c3bca84990e2d3e690bdde244e15625869354
Glenn Chiang <glennchiang24@gmail.com>
2023-10-17 22:24:27 +0800

    Added router.push click handler to home link on tipnav

--- src/components/nav/TopNav.tsx
+++ src/components/nav/TopNav.tsx
@@ -12,6 +12,7 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 import { useEffect, useRef, useState } from "react";
+import { useRouter } from "next/router";
 
 export default function TopNav({
   toggleSidebar,
@@ -44,6 +45,8 @@ export default function TopNav({
     return () => document.removeEventListener("click", hideDropdown);
   }, [dropdownIsOpen]);
 
+  const router = useRouter()
+
   return (
     <nav className="bg-white text-xl p-2 pr-4 flex justify-between items-center fixed h-16 w-screen top-0 left-0 z-20 font-medium shadow-md">
       <div className="flex gap-2">
@@ -53,7 +56,7 @@ export default function TopNav({
         >
           <FontAwesomeIcon icon={faBars} />
         </button>
-        <Link href={"/"} className="flex gap-2 items-center">
+        <Link onClick={() => router.push('/')} href={"/"} className="flex gap-2 items-center">
           <DolphinIcon />
           Dolphin
         </Link>
