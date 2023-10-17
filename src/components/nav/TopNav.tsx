880a34e18955809da3bfeeb0e0974e77f0d3aad6
Glenn Chiang <glennchiang24@gmail.com>
2023-10-17 22:48:48 +0800

    added prefetch:false to homelink

--- src/components/nav/TopNav.tsx
+++ src/components/nav/TopNav.tsx
@@ -12,12 +12,11 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 import { useEffect, useRef, useState } from "react";
-import { useRouter } from "next/navigation";
 
 export default function TopNav({
   toggleSidebar,
 }: {
-  toggleSidebar: () => void;
+  toggleSidebar?: () => void;
 }) {
   const [dropdownIsOpen, setDropdownIsOpen] = useState(false);
   const toggleDropdown = () => setDropdownIsOpen((prev) => !prev);
@@ -45,8 +44,6 @@ export default function TopNav({
     return () => document.removeEventListener("click", hideDropdown);
   }, [dropdownIsOpen]);
 
-  const router = useRouter()
-
   return (
     <nav className="bg-white text-xl p-2 pr-4 flex justify-between items-center fixed h-16 w-screen top-0 left-0 z-20 font-medium shadow-md">
       <div className="flex gap-2">
@@ -56,10 +53,10 @@ export default function TopNav({
         >
           <FontAwesomeIcon icon={faBars} />
         </button>
-        <div onClick={() => router.push('/')} className="flex gap-2 items-center">
+        <Link prefetch={false} href={"/"} className="flex gap-2 items-center">
           <DolphinIcon />
           Dolphin
-        </div>
+        </Link>
       </div>
       <div onClick={toggleDropdown} className="relative">
         <div
